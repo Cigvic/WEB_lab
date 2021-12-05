22a4cb51cd847356a0eeac7efb1dc66f79ba27c3
Цигвинцев Илья <70623531+Cigvic@users.noreply.github.com>
2021-12-05 19:50:45 +0500

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,10 @@
   echo (file_get_contents("Site.html"))
    $C=$_GET["C"];
    switch($C){
-    case "1": echo("faq.html"); break;   
-    case "2": echo(file_get_contents("faq.html")); break;
-    case "3": echo(file_get_contents("feedback.html")); break;
+    case "1": echo("faq.html"); break;
+    case "2": echo("faq.html"); break; 
+    case "3": echo(file_get_contents("faq.html")); break;
+    case "4": echo(file_get_contents("feedback.html")); break;
     default: echo("magic");  
   }
  
