9d15b75dcb19e99eac3f49de8d190dc3c9e53221
Цигвинцев Илья <70623531+Cigvic@users.noreply.github.com>
2021-12-05 19:50:08 +0500

    Update index.php

--- index.php
+++ index.php
@@ -2,14 +2,16 @@
   echo (file_get_contents("Site.html"))
    $C=$_GET["C"];
    switch($C){
-    case "1": echo(file_get_contents("faq.html")); break;
-    case "2": echo(file_get_contents("feedback.html")); break;
+    case "1": echo("faq.html"); break;   
+    case "2": echo(file_get_contents("faq.html")); break;
+    case "3": echo(file_get_contents("feedback.html")); break;
     default: echo("magic");  
   }
  
    $O=$_GET["O"];
    switch($O){
-    case "1": echo(file_get_contents("minigame.html")); break;
+    case "1": echo("minigame.html"); break;   
+    case "2": echo(file_get_contents("minigame.html")); break;
     default: echo("magic");   
   }
 ?>
