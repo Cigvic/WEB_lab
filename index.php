7d6cec6ef40e1eff63b6b54150ce0840b1bbe3a2
Цигвинцев Илья <70623531+Cigvic@users.noreply.github.com>
2021-12-05 19:33:58 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,3 @@
 <?
   echo (file_get_contents("Site.html"))
-  $C=$_GET["C"];
-  switch($C){
-    case "1": echo(file_get_contents("faq.html")); break;
-    case "2": echo(file_get_contents("feedback.html")); break;
-  }
 ?>
