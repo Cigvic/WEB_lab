896e8f42537014b6135e53b4d8859519be5430a7
Цигвинцев Илья <70623531+Cigvic@users.noreply.github.com>
2021-12-06 14:09:42 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,3 @@
 <?
-  echo (file_get_contents("Site.html"))
-  echo (file_get_contents("faq.html"))
-  echo (file_get_contents("feedback.html"))
-  echo (file_get_contents("minigame.html"))  
+  echo (file_get_contents("Site.html"))  
 ?>
