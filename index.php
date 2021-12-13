bd1c4d653e8faf8916d5de7458894b6ca1c7de3e
Цигвинцев Илья <70623531+Cigvic@users.noreply.github.com>
2021-12-13 17:07:57 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,14 @@
-    
 <?php 
 $tmp=file_get_contents("6lab.html");
 $objects="";
-function load_content($s)
-{
-  $GLOBALS['content'].=$s;
-}
 $C = $_GET["C"];
   switch($C) {
     case "1": $objects=file_get_contents("faq.html"); break;
     case "2": $objects=file_get_contents("feedback.html"); break;
     case "3": $objects=file_get_contents("minigame.html"); break;
+    default:  $objects=file_get_contents("faq.html");
   }
 $tmp = str_replace("{objects}"), $objects, $tmp);
 echo $tmp;  
-
 ?>
 
